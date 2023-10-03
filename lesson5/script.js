17adb592290cce7b0122c611e8f8ee2b92180c7e
Jeremy Kung <jeremykung@gmail.com>
2023-10-03 11:23:32 +0300

    added functions part 2

---
+++ lesson5/script.js
@@ -0,0 +1,23 @@
+// Functions Part 2
+// Parameters
+// Arguments
+
+// parameters > flavour, amount
+function makePancake(flavour) {
+    console.log(`making ${flavour} pancake`)
+}
+
+makePancake("blueberry", 5)
+
+// parameters > flavour, amount
+function makeManyPancakes(flavour, amount) {
+    console.log(`making ${amount} ${flavour} pancakes`)
+}
+
+// arguments > blueberry, apple
+makeManyPancakes("apple", 4)
+
+
+// 1. Create a function that says hi to someone. Someone should be a parameter. The output should be: "hi georg" or "hi alex".
+// 2. Create a function that doubles any number you give it. For example, if you pass 3 as an argument, it will output 6.
+// 3. Create a function that adds 1 to any number it gets. For example, if you pass 8 as an argument, it will output 9.
