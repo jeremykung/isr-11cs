827a9b3cad47d3f956e866973406c7c4c1ac529b
jkung <jeremykung@gmail.com>
2024-01-25 15:14:41 +0200

    added array methods

---
+++ lesson12/script.js
@@ -0,0 +1,21 @@
+/*
+
+Array Methods 💪🔥
+
+pop()      removes last element of array
+push()     adds to back of array
+unshift()  adds to the front of array
+filter()   filters out elements of array
+slice()    removes element(s) by index
+map()      manipulates and store as new array
+forEach()  does a function for each array
+
+*/
+
+const randomNumbers = [2, 5, 1, 4, 3]
+
+randomNumbers.pop()
+randomNumbers.push(7)
+randomNumbers.unshift("sergei")
+
+console.log(randomNumbers)
