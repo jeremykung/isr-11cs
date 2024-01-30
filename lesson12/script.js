b37484591dafe43510e7bd28d48ef39f0f5162b9
jkung <jeremykung@gmail.com>
2024-01-30 11:34:33 +0200

    updated with forEach() and filter()

--- lesson12/script.js
+++ lesson12/script.js
@@ -6,17 +6,60 @@ pop()      removes last element of array
 push()     adds to back of array
 unshift()  adds to the front of array
 slice()    removes element(s) by index
+forEach()  does a function for each array
 filter()   filters out elements of array
 map()      manipulates and store as new array
-forEach()  does a function for each array
 
 */
-//                     0  1  2  3  4
-const randomNumbers = [2, 5, 1, 4, 3]
+
+// element              0  1  2  3  4
+const randomNumbers1 = [2, 5, 1, 4, 3]
 const randomNumbers2 = [2, 5, 1, 7, 8]
 
-const result = randomNumbers.slice(2, 4)
-console.log(result)     // [1, 4]
+
+randomNumbers1.forEach(function(currentElement) {
+    console.log(currentElement)
+})
+
+
+// 1. Create an array of drinks, use forEach() to log "I like drinking _____" where ____ is each drink in the array
+
+// 2. Using only the forEach() method, create a new array that is only numbers less than 5
+
+
+
+
+
+// const filteredArray = randomNumbers1.filter(overThree)
+// console.log(filteredArray)
+
+// function overThree(element) {
+//     return element > 3
+// }
+
+
+
+
+// filter the array so that it returns ONLY number 2
+
+
+
+
+
+
+
+
+// for each element in the array, subtract 2 then multiply by 5
+// then log the answer
+
+
+
+
+
+
+
+// const result = randomNumbers1.slice(2, 4)
+// console.log(result)     // [1, 4]
 
 // Create a function called "replaceAllMiddleWithThree"
 // this function cuts out all the middle elements except the first and last, and replaces the middle with just 3
@@ -25,14 +68,14 @@ console.log(result)     // [1, 4]
 // for example [2,6,2,7,9] => [2,3,9]
 
 
-function removeOne(theArray) {
-    const firstPop = theArray.pop()
-    const secondPop = theArray.pop()
-    theArray.pop() // removes the 1
-    theArray.push(secondPop)
-    theArray.push(firstPop)
-    console.log(theArray)
-}
+// function removeOne(theArray) {
+//     const firstPop = theArray.pop()
+//     const secondPop = theArray.pop()
+//     theArray.pop() // removes the 1
+//     theArray.push(secondPop)
+//     theArray.push(firstPop)
+//     console.log(theArray)
+// }
 
 // removeOne(randomNumbers)
 // removeOne(randomNumbers2)
